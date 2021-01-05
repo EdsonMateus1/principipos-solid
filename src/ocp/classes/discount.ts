88d3eaac1acc6c7b84cb58cd402eec3c91615e83
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-05 08:36:45 -0300

    ocp

--- src/ocp/classes/discount.ts
+++ src/ocp/classes/discount.ts
@@ -1,16 +1,13 @@
 export abstract class Discount {
-  abstract calculate(value: number): number;
+  protected discount = 0;
+  calculate(price: number): number {
+    return price - (price * this.discount) / 100;
+  }
 }
 
 export class FiftyPercentDiscount extends Discount {
-  private readonly discount = 50;
-  calculate(price: number): number {
-    return price - (price * this.discount) / 100;
-  }
+  protected readonly discount = 50;
 }
 export class TenPercentDiscount extends Discount {
-  private readonly discount = 10;
-  calculate(price: number): number {
-    return price - (price * this.discount) / 100;
-  }
+  protected readonly discount = 10;
 }
