89c87e40db712e475842b31487937e08b538691d
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-06 10:54:31 -0300

    lsp

--- src/lsp/classes/discount.ts
+++ src/lsp/classes/discount.ts
@@ -1,6 +1,7 @@
 export abstract class Discount {
   protected discount = 0;
-  calculate(price: number): unknown { //violando o principio
+  calculate(price: number): unknown {
+    //violando o principio
     return price - (price * this.discount) / 100;
   }
 }
@@ -8,9 +9,17 @@ export abstract class Discount {
 export class FiftyPercentDiscount extends Discount {
   protected readonly discount = 50;
   calculate(price: number): string {
-    return "Liskov"
+    return "Liskov";
   }
 }
 export class TenPercentDiscount extends Discount {
   protected readonly discount = 10;
+}
+
+
+// quebrado o principio de Liskov pois estou mudando o comportamento padrao da classe base/pai
+export class NoDiscount extends Discount {
+  calculate(price: number): number {
+    return price;
+  }
 }
